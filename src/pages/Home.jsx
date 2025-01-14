349f5866dee7440e5e09dcaaef0108532232fa83
KAkos27 <akoskosztolanyi@gmail.com>
2025-01-14 14:01:35 +0100

    swiper letoltes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import Header from "../components/Header";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
     </div>
   );
 };
