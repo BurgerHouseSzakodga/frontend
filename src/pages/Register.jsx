7a631f1571383f8db14b1decf391dc407cbbdd25
lajkogege <lajkogege@gmail.com>
2025-01-14 09:53:03 +0100

    css imort registerben

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-
+import '../sass/pages/register.css' 
 import { AuthContext } from "../context/contexts";
 
 const Register = () => {
@@ -7,8 +7,9 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [address, setAddress]=useState("");
 
-  const { register } = useContext(AuthContext);
+  const { register, registerError } = useContext(AuthContext);
 
   const handleRegister = (event) => {
     event.preventDefault();
@@ -18,6 +19,7 @@ const Register = () => {
       email,
       password,
       password_confirmation: passwordConfirmation,
+      address
     };
 
     register(payload);
@@ -35,6 +37,7 @@ const Register = () => {
             name="name"
           />
         </div>
+        {registerError.name && <p>{registerError.name}</p>}
         <div>
           <label htmlFor="email">Email cím:</label>
           <input
@@ -44,6 +47,7 @@ const Register = () => {
             name="email"
           />
         </div>
+        {registerError.email && <p>{registerError.email}</p>}
         <div>
           <label htmlFor="password">Jelszó:</label>
           <input
@@ -53,6 +57,7 @@ const Register = () => {
             name="password"
           />
         </div>
+        {registerError.password && <p>{registerError.password}</p>}
         <div>
           <label htmlFor="password-confirmation">Jelszó mégegyszer:</label>
           <input
@@ -62,6 +67,17 @@ const Register = () => {
             name="password-confirmation"
           />
         </div>
+       
+         <div>
+          <label htmlFor="address">Szállitási cim:</label>
+          <input
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            type="text"
+            name="address"
+          />
+        </div>
+        {registerError.address && <p>{registerError.address}</p>}
         <div>
           <input type="submit" value="Regisztráció" />
         </div>
