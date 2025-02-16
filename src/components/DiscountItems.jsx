3487569a3ce2caddf66ebf5fb30f42b9f7e7baf6
KAkos27 <akoskosztolanyi@gmail.com>
2025-02-16 17:04:55 +0100

    akciojav

--- src/components/DiscountItems.jsx
+++ src/components/DiscountItems.jsx
@@ -1,29 +1,27 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules';
-import { Link, useNavigate } from 'react-router-dom';
-import { MenuItemContext } from '../context/contexts';
+import { useContext, useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import { Link } from "react-router-dom";
+import { MenuItemContext } from "../context/contexts";
 import rightIcon from "/assets/right.svg";
 import leftIcon from "/assets/left.svg";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import '../sass/components/discounts-item.css';
-
+import "swiper/css";
+import "swiper/css/navigation";
+import "../sass/components/discounts-item.css";
 
 function DiscountItems() {
   const { discountedItems } = useContext(MenuItemContext);
   const [items, setItems] = useState([]);
   const swiperRef = useRef(null);
-  const navigate = useNavigate();
 
   useEffect(() => {
     setItems(discountedItems);
   }, [discountedItems]);
 
   const handleImage = (direction) => {
-    if (direction === 'next') {
+    if (direction === "next") {
       swiperRef.current.swiper.slideNext();
-    } else if (direction === 'prev') {
+    } else if (direction === "prev") {
       swiperRef.current.swiper.slidePrev();
     }
   };
@@ -32,14 +30,13 @@ function DiscountItems() {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  const calculateDiscountPercentage = (originalPrice, discountedPrice) => {
-    return Math.round(((originalPrice - discountedPrice) / originalPrice) * 100);
-  };
-
   return (
     <div className="discount-item-container">
-      <h1 className='cim'>Akciós termékek</h1>
-      <button className="swiper-button prev" onClick={() => handleImage('prev')}>
+      <h1 className="cim">Akciós termékek</h1>
+      <button
+        className="swiper-button prev"
+        onClick={() => handleImage("prev")}
+      >
         <img src={leftIcon} alt="Previous" />
       </button>
 
@@ -53,20 +50,18 @@ function DiscountItems() {
           disableOnInteraction: false,
         }}
         navigation={{
-          nextEl: '.swiper-button.next',
-          prevEl: '.swiper-button.prev',
+          nextEl: ".swiper-button.next",
+          prevEl: ".swiper-button.prev",
         }}
       >
         {items.map((item) => (
           <SwiperSlide key={item.id}>
             <div className="item-card">
-              <div className="discount-badge">
-                -{calculateDiscountPercentage(item.price, item.price - item.discount_amount)}%
-              </div>
+              <div className="discount-badge">-{item.discount_amount}%</div>
               <img src={item.image_path} alt={item.name} />
               <h3>{capitalizeFirstLetter(item.name)}</h3>
               <p className="original-price">{item.price} Ft</p>
-              <p className="discounted-price">{item.price - item.discount_amount} Ft</p>
+              <p className="discounted-price">{item.actual_price} Ft</p>
               <Link to={`/item/${item.id}`} className="basket-button">
                 Rendelés
               </Link>
@@ -75,7 +70,10 @@ function DiscountItems() {
         ))}
       </Swiper>
 
-      <button className="swiper-button next" onClick={() => handleImage('next')}>
+      <button
+        className="swiper-button next"
+        onClick={() => handleImage("next")}
+      >
         <img src={rightIcon} alt="Next" />
       </button>
     </div>
